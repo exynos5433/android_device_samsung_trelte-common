c7f8f7a720f151d644c5c50de40a12464e760d97
Anan Jaser <ananjaser@gmail.com>
2020-10-08 18:40:00 +0400

    trelte-common: Build binderized trelte sensors service

--- device-common.mk
+++ device-common.mk
@@ -299,7 +299,8 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-impl.tre \
+    android.hardware.sensors@1.0-service.tre \
     android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service \
     sensors.exynos5
