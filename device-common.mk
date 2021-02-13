aa3a150c6d8182001f9fda0be4360527ecda67e9
Anan Jaser <ananjaser@gmail.com>
2021-02-14 01:57:55 +0400

    trelte-common: remove unused audio_policy.conf
    Change-Id: If2a806883cd340c01b6e4b237685746d7565882d

--- device-common.mk
+++ device-common.mk
@@ -34,9 +34,7 @@ PRODUCT_PACKAGES += \
     tinyplay
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio/audio_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy.conf \
     $(LOCAL_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
-    $(LOCAL_PATH)/configs/audio/audio_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy.conf \
     $(LOCAL_PATH)/configs/audio/silence.wav:$(TARGET_COPY_OUT_VENDOR)/etc/silence.wav \
     $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
