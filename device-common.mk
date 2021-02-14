5929b92a2e6c830283d256647c6497d3b79beb44
Anan Jaser <ananjaser@gmail.com>
2021-02-14 21:39:13 +0400

    trelte-commo: move lineage-sdk into overlays
    * merge setting overlays
    
    Change-Id: I59b138824a11e408cf34f598b0e3a8466538fab8

--- device-common.mk
+++ device-common.mk
@@ -227,8 +227,7 @@ PRODUCT_PACKAGES += \
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 # Permissions
 PRODUCT_COPY_FILES += \
