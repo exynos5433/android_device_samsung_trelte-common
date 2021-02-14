f0adfee2be7ac44e8221dc8e7db711c8299f5b99
Anan Jaser <ananjaser@gmail.com>
2021-02-15 00:11:07 +0400

    Revert "trelte-common: enable Wi-Fi Aware"
    This reverts commit 73c070b38aea85a41e8ad4920b095d6132992818.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -286,7 +286,6 @@ WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/dhd/parameters/firmware_path"
 WIFI_DRIVER_FW_PATH_STA          := "/vendor/etc/wifi/bcmdhd_sta.bin"
 WIFI_DRIVER_FW_PATH_AP           := "/vendor/etc/wifi/bcmdhd_apsta.bin"
 #WIFI_BAND                        := 802_11_ABG
-WIFI_HIDL_FEATURE_AWARE          := true
 WPA_SUPPLICANT_USE_HIDL          := true
 
 # WiFi Configs
