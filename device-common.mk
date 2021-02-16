8b78aaf7dc9a468cdcc74e005094b556641c751a
Anan Jaser <ananjaser@gmail.com>
2021-02-16 11:49:10 +0400

    trelte-common: build our powerHAL
    Change-Id: I4854c5da581b00e2f0f2e6738e25a29339c2831d

--- device-common.mk
+++ device-common.mk
@@ -277,7 +277,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service.exynos
+    android.hardware.power@1.0-service.universal5433
 
 # Ramdisk
 PRODUCT_PACKAGES += \
