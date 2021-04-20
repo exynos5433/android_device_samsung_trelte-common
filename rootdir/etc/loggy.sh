fb4535d4eb66ba97114a0b5f2eedf2bcd1d0b25a
Anan Jaser <ananjaser@gmail.com>
2021-04-21 00:17:10 +0400

    trelte-common: ramdisk: rsync tombstones

--- rootdir/etc/loggy.sh
+++ rootdir/etc/loggy.sh
@@ -4,5 +4,5 @@
 date=`date +%F_%H-%M-%S`
 logcat -b all -v time -f  /cache/logcat_${date}.txt &
 cat /dev/kmsg > /cache/kmsg_${date}.txt &
-mv /data/tombstones > /cache/tombstones_${date} &
+rsync -ar /data/tombstones > /cache/tombstones/ &
 cat /proc/last_kmsg > /cache/last_kmsg${date}.txt
