832beb99309d0e02b192a398b80bf28d0d5d7faf
Anan Jaser <ananjaser@gmail.com>
2021-07-25 23:51:30 +0400

    trelte-common: remove get_imsi_from_sim
    * this seems to be a CDMA property

--- system_prop.mk
+++ system_prop.mk
@@ -100,7 +100,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.ril.gprsclass=10 \
     persist.data.netmgrd.qos.enable=false \
     ro.use_data_netmgrd=false \
-    ro.telephony.get_imsi_from_sim=true \
     ro.ril.force_eri_from_xml=true \
     ro.telephony.mms_data_profile=5 \
     ro.gsm.sim.operator.iso-country=false
