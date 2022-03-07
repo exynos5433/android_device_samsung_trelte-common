1c8be20e59770667236898c4474aa2d64dd125b6
Anan Jaser <ananjaser@gmail.com>
2022-03-07 21:10:24 +0400

    trelte-common: fingerprint: move to vendor
    Change-Id: Iba4e52b4e988be5ecbf8a07c7125527ce1fc83e2

--- fingerprint/Android.mk
+++ fingerprint/Android.mk
@@ -20,7 +20,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := fingerprint.universal5433
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_SRC_FILES := fingerprint.c
 LOCAL_SHARED_LIBRARIES := liblog libcutils
 LOCAL_MODULE_TAGS := optional
