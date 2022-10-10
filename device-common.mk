0a1725ef48aab4cf2f105653b26cf858e3f2d756
Anan Jaser <ananjaser@gmail.com>
2022-10-11 01:16:00 +0400

    trelte-common: move NFC packages to main device.mk
    Change-Id: I30aa16beb97fd001a6359aba7b743ac5316d439d

--- device-common.mk
+++ device-common.mk
@@ -1,9 +1,6 @@
 
 PRODUCT_CHARACTERISTICS := phonemedia
 
-# Inherit products
-include $(LOCAL_PATH)/product/*.mk
-
 # Common inherits
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
@@ -197,6 +194,30 @@ PRODUCT_PACKAGES += \
     mcDriverDaemon \
     libMcClient \
     libMcRegistry
+    
+# NFC
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/configs/nfc/nfcee_access.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/nfcee_access.xml \
+	$(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
+	$(LOCAL_PATH)/configs/nfc/libnfc-sec.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-sec.conf \
+	$(LOCAL_PATH)/configs/nfc/libnfc-sec-hal.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-sec-hal.conf
+
+PRODUCT_PACKAGES += \
+	libnfc-nci \
+	libnfc_nci_jni \
+	NfcNci \
+	Tag \
+	com.android.nfc_extras \
+	android.hardware.nfc@1.0-impl
+
+PRODUCT_COPY_FILES += \
+	frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.nfc.hce.xml \
+	frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.nfc.xml \
+	frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.android.nfc_extras.xml
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.nfc.fw_dl_on_boot=false \
+	ro.nfc.port="I2C"
 
 # Lights
 PRODUCT_PACKAGES += \
